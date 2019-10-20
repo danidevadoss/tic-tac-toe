1a26773a747aa53d483cfb34cbfbe98909403a49
sudarsant <sudarsant@stgit.com>
2019-10-20 14:36:32 +0530

    Add Screen Navigation

--- app/game/Game.js
+++ app/game/Game.js
@@ -12,42 +12,7 @@ export default class Game extends Component {
     }
 
     render() {
-        return (
-            <>
-                {this.state.landingScreen ?
-                    <View style={{ flex: 1, flexDirection: "column", backgroundColor: '#24478f' }}>
-                        <Text style={{ fontSize: 50, textAlign: 'center', marginTop: 100, color: 'white' }}>TIC TAC TOE</Text>
-                        <Text style={{ fontSize: 50, textAlign: 'center' }}>  ❌ ⭕ </Text>
-                        <TouchableOpacity onPress={this.start2PlayerOfflineGame} >
-                            <View style={{
-                                backgroundColor: '#00CA4E', justifyContent: 'center', alignItems: 'center',
-                                marginLeft: 30, marginRight: 30, marginTop: 50, borderRadius: 60, height: 80
-                            }}
-                            >
-                                <Text style={{ color: 'white', fontSize: 25 }}>Two Player Offline </Text>
-                            </View>
-                        </TouchableOpacity>
-                        <View style={{
-                            backgroundColor: '#8c8c8c', justifyContent: 'center', alignItems: 'center',
-                            marginLeft: 30, marginRight: 30, marginTop: 50, borderRadius: 60, height: 80
-                        }}
-                        >
-                            <Text style={{ color: 'white', fontSize: 25 }}>Single Player 🔜 </Text>
-                        </View>
-                        <View style={{
-                            backgroundColor: '#8c8c8c', justifyContent: 'center', alignItems: 'center',
-                            marginLeft: 30, marginRight: 30, marginTop: 50, borderRadius: 60, height: 80
-                        }}
-                        >
-                            <Text style={{ color: 'white', fontSize: 25 }}>Two Player Online 🔜</Text>
-                        </View>
-                    </View>
-                    :
-                    <TicTacToeApp></TicTacToeApp>
-                }
-
-            </>
-        );
+        return (<TicTacToeApp></TicTacToeApp>);
     }
 
     start2PlayerOfflineGame = () => {
